ffbb581fbac8f47661e2aaf749dd1b1ee183fddf
Yipeng Sun <syp@umd.edu>
2019-10-19 03:08:32 -0400

    Bump version.

--- pyTuplingUtils/__init__.py
+++ pyTuplingUtils/__init__.py
@@ -2,7 +2,7 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Sat Oct 19, 2019 at 03:05 AM -0400
+# Last Change: Sat Oct 19, 2019 at 03:08 AM -0400
 
 name = 'pyTuplingUtils'
-version = '0.1.5'
+version = '0.1.6'
