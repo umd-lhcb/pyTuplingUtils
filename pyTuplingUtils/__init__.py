029d31646c68531a6a604125bccbf184f7e50e25
Yipeng Sun <syp@umd.edu>
2019-10-19 03:05:29 -0400

    Fix: Edit typo.

--- pyTuplingUtils/__init__.py
+++ pyTuplingUtils/__init__.py
@@ -2,7 +2,7 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Sat Oct 19, 2019 at 03:04 AM -0400
+# Last Change: Sat Oct 19, 2019 at 03:05 AM -0400
 
 name = 'pyTuplingUtils'
-version = '0.1.4'
+version = '0.1.5'
