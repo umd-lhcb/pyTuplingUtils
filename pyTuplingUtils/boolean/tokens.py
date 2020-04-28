a741be55e5bb6e1af807ac699ed7d33f6e84bb7d
Yipeng Sun <syp@umd.edu>
2020-04-29 04:29:52 +0800

    Consistent license notice.

--- pyTuplingUtils/boolean/tokens.py
+++ pyTuplingUtils/boolean/tokens.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 #
 # Author: Yipeng Sun
-# License: GPLv2
-# Last Change: Wed Apr 29, 2020 at 03:53 AM +0800
+# License: BSD 2-clause
+# Last Change: Wed Apr 29, 2020 at 04:29 AM +0800
 
 from pyparsing import Suppress, Literal, Word, alphanums, nums
 
