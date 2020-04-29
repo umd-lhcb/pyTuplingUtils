c73847b3c00af7649b1049e4675984559771c1d0
Yipeng Sun <syp@umd.edu>
2020-04-30 03:44:42 +0800

    WIP #1: Rewrite basic arithmetic in lark.

---
+++ pyTuplingUtils/boolean/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+#
+# Author: Yipeng Sun
+# License: BSD 2-clause
+# Last Change: Thu Apr 30, 2020 at 03:45 AM +0800
+
+import pyTuplingUtils.boolean.syntax
---
+++ pyTuplingUtils/boolean/syntax.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+#
+# Author: Yipeng Sun
+# License: BSD 2-clause
+# Last Change: Thu Apr 30, 2020 at 03:37 AM +0800
+
+from lark import Lark
+
+
+boolean_grammer = '''
+    ?start: sum
+
+    ?sum: product
+        | sum "+" product   -> add
+        | sum "-" product   -> sub
+
+    ?product: atom
+        | product "*" atom  -> mul
+        | product "/" atom  -> div
+
+    ?atom: NUMBER           -> num
+        | "-" atom          -> neg
+        | NAME              -> var
+        | "(" sum ")"
+
+    %import common.NUMBER
+    %import common.WS_INLINE
+    %import common.CNAME -> NAME
+
+    %ignore WS_INLINE
+'''
+
+boolean_parser = Lark(boolean_grammer, parser='lalr')
