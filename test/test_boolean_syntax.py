f73ff4655f90420d682c048e55c7950b37946d82
Yipeng Sun <syp@umd.edu>
2020-04-29 17:32:44 +0800

    WIP #1: More tests on the parsing of basic arithmetic.

--- test/test_boolean_syntax.py
+++ test/test_boolean_syntax.py
@@ -2,7 +2,7 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Wed Apr 29, 2020 at 05:18 PM +0800
+# Last Change: Wed Apr 29, 2020 at 05:32 PM +0800
 
 import unittest
 
@@ -15,6 +15,28 @@ class AddSubTest(unittest.TestCase):
 
         self.assertEqual(result[0].operator, '+')
         self.assertEqual(result[0].operands, ['a', '3'])
+
+    def test_mul(self):
+        result = ptu.boolean.EXPR.parseString('a * 3').asList()
+
+        self.assertEqual(result[0].operator, '*')
+        self.assertEqual(result[0].operands, ['a', '3'])
+
+    def test_add_mul_precedence1(self):
+        result = ptu.boolean.EXPR.parseString('a * 3+b').asList()
+
+        self.assertEqual(result[0].operator, '+')
+        self.assertEqual(result[0].operands[1], 'b')
+        self.assertEqual(result[0].operands[0].operator, '*')
+        self.assertEqual(result[0].operands[0].operands, ['a', '3'])
+
+    def test_add_mul_precedence2(self):
+        result = ptu.boolean.EXPR.parseString('a * (3+b)').asList()
+
+        self.assertEqual(result[0].operator, '*')
+        self.assertEqual(result[0].operands[0], 'a')
+        self.assertEqual(result[0].operands[1].operator, '+')
+        self.assertEqual(result[0].operands[1].operands, ['3', 'b'])
 
 
 if __name__ == '__main__':
