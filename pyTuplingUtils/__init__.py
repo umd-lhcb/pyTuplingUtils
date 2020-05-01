9edd55dd5bacada8011d4f4034d9e572d44fc031
Yipeng Sun <syp@umd.edu>
2020-05-01 15:30:43 +0800

    Remove the '__version__' var to hopefully have correct tag on PyPI.

--- pyTuplingUtils/__init__.py
+++ pyTuplingUtils/__init__.py
@@ -2,14 +2,11 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Fri May 01, 2020 at 02:47 PM +0800
+# Last Change: Fri May 01, 2020 at 03:30 PM +0800
 
 name = 'pyTuplingUtils'
 author = 'Yipeng Sun <syp@umd.edu>'
 version = '0.0.1'
-
-__author__ = author
-__version__ = version
 
 import pyTuplingUtils.boolean
 import pyTuplingUtils.io
