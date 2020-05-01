1916a24c45797ca1eee0ef7dace407397ada73de
Yipeng Sun <syp@umd.edu>
2020-05-01 17:09:43 +0800

    Remove unneeded author info.

--- pyTuplingUtils/__init__.py
+++ pyTuplingUtils/__init__.py
@@ -2,10 +2,9 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Fri May 01, 2020 at 04:58 PM +0800
+# Last Change: Fri May 01, 2020 at 05:09 PM +0800
 
 __name__ = 'pyTuplingUtils'
-__author__ = 'Yipeng Sun <syp@umd.edu>'
 __version__ = '0.0.1'
 
 import pyTuplingUtils.boolean
