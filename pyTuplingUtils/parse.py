b31878f3109e00e8fdcbede4ea5c395aab71a638
Yipeng Sun <syp@umd.edu>
2021-06-07 03:20:49 +0200

    Move more default flags to the parsers.

--- pyTuplingUtils/parse.py
+++ pyTuplingUtils/parse.py
@@ -2,7 +2,7 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Mon Jun 08, 2020 at 11:10 PM +0800
+# Last Change: Mon Jun 07, 2021 at 03:19 AM +0200
 
 from argparse import ArgumentParser
 
@@ -11,16 +11,20 @@ def single_ntuple_parser_no_output(descr, parser=None):
     parser = ArgumentParser(description=descr) if parser is None else parser
 
     parser.add_argument('-n', '--ref',
-                        nargs='?',
                         required=True,
                         help='''
 path to reference ntuple.''')
 
     parser.add_argument('-t', '--ref-tree',
-                        nargs='?',
                         required=True,
                         help='''
 tree name for the reference ntuple.''')
+
+    parser.add_argument('--bins',
+                        type=int,
+                        default=25,
+                        help='''
+specify binning.''')
 
     return parser
 
@@ -29,16 +33,29 @@ def single_ntuple_parser(descr, parser=None):
     parser = single_ntuple_parser_no_output(descr) if parser is None else parser
 
     parser.add_argument('-o', '--output',
-                        nargs='?',
                         required=True,
                         help='''
 path to output file.''')
 
     parser.add_argument('-b', '--ref-branch',
-                        nargs='?',
                         required=True,
                         help='''
 branch name(s) in reference ntuple. may be separated by ",".''')
+
+    parser.add_argument('-l', '--ref-label',
+                        required=True,
+                        help='''
+specify label for reference branch.''')
+
+    parser.add_argument('--xlabel',
+                        default=None,
+                        help='''
+specify xlabel.''')
+
+    parser.add_argument('--ylabel',
+                        default='Number of candidates',
+                        help='''
+specify ylabel.''')
 
     return parser
 
@@ -47,13 +64,11 @@ def double_ntuple_parser_no_output(descr, parser=None):
     parser = single_ntuple_parser_no_output(descr) if parser is None else parser
 
     parser.add_argument('-N', '--comp',
-                        nargs='?',
                         required=True,
                         help='''
 path to comparison ntuple.''')
 
     parser.add_argument('-T', '--comp-tree',
-                        nargs='?',
                         required=True,
                         help='''
 tree name for the comparison ntuple.''')
@@ -67,9 +82,13 @@ def double_ntuple_parser(descr, parser=None):
         parser = single_ntuple_parser(descr, parser)
 
     parser.add_argument('-B', '--comp-branch',
-                        nargs='?',
                         required=True,
                         help='''
 branch name(s) in comparison ntuple. may be separated by ",".''')
+
+    parser.add_argument('-L', '--comp-label',
+                        required=True,
+                        help='''
+specify label for comparison branch.''')
 
     return parser
