ab3cc4ebd4ca44d5a09c5382e9aaa12619d69379
Yipeng Sun <syp@umd.edu>
2021-06-12 21:05:28 +0200

    WIP #7: Prepare argument parsers for general plotters.

--- pyTuplingUtils/argparse.py
+++ pyTuplingUtils/argparse.py
@@ -2,10 +2,49 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Mon Jun 07, 2021 at 03:19 AM +0200
+# Last Change: Sat Jun 12, 2021 at 06:26 PM +0200
 
-from argparse import ArgumentParser
+import sys
+from argparse import Action, ArgumentParser
 
+
+###########################
+# Argument parser actions #
+###########################
+
+class DataRangeAction(Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        if values is None:
+            setattr(namespace, self.dest, [])
+
+        if len(values) % 2 != 0:
+            print('Odd number of min, max pairs!')
+            sys.exit(255)
+
+        values = [float(v) for v in values]
+        min_max_pairs = self.divide_list_in_chunk(values)
+        setattr(namespace, self.dest, min_max_pairs)
+
+    @staticmethod
+    def divide_list_in_chunk(lst, chunk_size=2):
+        return [lst[i:i + chunk_size] for i in range(0, len(lst), chunk_size)]
+
+
+class DataPairAction(Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        if values is None:
+            setattr(namespace, self.dest, [])
+
+        try:
+            val_min, val_max = [float(i) for i in values]
+            setattr(namespace, self.dest, [val_min, val_max])
+        except ValueError:
+            print('Invalid value for pair: {}'.format(values))
+
+
+##############################
+# Bare-bone argument parsers #
+##############################
 
 def single_ntuple_parser_no_output(descr, parser=None):
     parser = ArgumentParser(description=descr) if parser is None else parser
@@ -29,37 +68,6 @@ specify binning.''')
     return parser
 
 
-def single_ntuple_parser(descr, parser=None):
-    parser = single_ntuple_parser_no_output(descr) if parser is None else parser
-
-    parser.add_argument('-o', '--output',
-                        required=True,
-                        help='''
-path to output file.''')
-
-    parser.add_argument('-b', '--ref-branch',
-                        required=True,
-                        help='''
-branch name(s) in reference ntuple. may be separated by ",".''')
-
-    parser.add_argument('-l', '--ref-label',
-                        required=True,
-                        help='''
-specify label for reference branch.''')
-
-    parser.add_argument('--xlabel',
-                        default=None,
-                        help='''
-specify xlabel.''')
-
-    parser.add_argument('--ylabel',
-                        default='Number of candidates',
-                        help='''
-specify ylabel.''')
-
-    return parser
-
-
 def double_ntuple_parser_no_output(descr, parser=None):
     parser = single_ntuple_parser_no_output(descr) if parser is None else parser
 
@@ -76,19 +84,61 @@ tree name for the comparison ntuple.''')
     return parser
 
 
-def double_ntuple_parser(descr, parser=None):
-    if parser is None:
-        parser = double_ntuple_parser_no_output(descr)
-        parser = single_ntuple_parser(descr, parser)
+def single_branch_parser_no_output(descr, parser=None):
+    parser = ArgumentParser(description=descr) if parser is None else parser
 
-    parser.add_argument('-B', '--comp-branch',
+    parser.add_argument('-n', '--ref',
                         required=True,
+                        action='append',
+                        nargs='+',
                         help='''
-branch name(s) in comparison ntuple. may be separated by ",".''')
+paths to reference ntuples.''')
 
-    parser.add_argument('-L', '--comp-label',
+    parser.add_argument('-t', '--ref-tree',
                         required=True,
+                        action='append',
+                        nargs='+',
                         help='''
-specify label for comparison branch.''')
+tree names for the reference ntuples.''')
+
+    parser.add_argument('-b', '--ref-branch',
+                        required=True,
+                        action='append',
+                        nargs='+',
+                        help='''
+branch names in reference trees.''')
+
+    parser.add_argument('--bins',
+                        type=int,
+                        default=25,
+                        help='''
+specify binning.''')
+
+    return parser
+
+
+def diff_branch_parser_no_output(descr, parser=None):
+    parser = single_branch_parser_no_output(descr) if parser is None else parser
+
+    parser.add_argument('-N', '--comp',
+                        required=True,
+                        action='append',
+                        nargs='+',
+                        help='''
+paths to comparison ntuples.''')
+
+    parser.add_argument('-T', '--comp-tree',
+                        required=True,
+                        action='append',
+                        nargs='+',
+                        help='''
+tree names for the comparison ntuples.''')
+
+    parser.add_argument('-b', '--comp-branch',
+                        required=True,
+                        action='append',
+                        nargs='+',
+                        help='''
+branch names in comparison trees.''')
 
     return parser
