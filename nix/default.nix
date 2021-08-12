87f7150cfb908a79cc6864d3900fddc67b845da5
Yipeng Sun <syp@umd.edu>
2021-08-13 01:40:11 +0200

    Make sure the version number is consistent.

--- nix/default.nix
+++ nix/default.nix
@@ -14,6 +14,10 @@ buildPythonPackage rec {
 
   src = builtins.path { path = ./..; name = pname; };
 
+  preBuild = ''
+    sed -i "s/0.0.1/${version}/" ./pyTuplingUtils/__init__.py
+  '';
+
   propagatedBuildInputs = [
     uproot
     lz4
