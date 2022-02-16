3a6c33fe8c414d719ee5712eb81e0bbfe6b884d9
Yipeng Sun <syp@umd.edu>
2022-02-16 13:26:15 -0500

    Fix: Make sure the 'read_*' functions work w/ opened ntuples.

--- pyTuplingUtils/io.py
+++ pyTuplingUtils/io.py
@@ -2,7 +2,7 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Wed Feb 16, 2022 at 01:02 PM -0500
+# Last Change: Wed Feb 16, 2022 at 01:24 PM -0500
 
 import numpy as np
 
@@ -11,15 +11,23 @@ from uproot import concatenate
 ARRAY_TYPE = 'np'
 
 
+def regulate_input(ntp, tree):
+    if isinstance(ntp, str):
+        return f'{ntp}:{tree}'
+    if isinstance(ntp, list):
+        return [regulate_input(i, tree) for i in ntp]
+    return ntp[tree]
+
+
 def read_branch(ntp, tree, branch, idx=None):
-    data = list(
-        concatenate(f'{ntp}:{tree}', branch, library=ARRAY_TYPE).values())[0]
+    data = list(concatenate(
+        regulate_input(ntp, tree), branch, library=ARRAY_TYPE).values())[0]
 
     return data if not idx else data[idx]
 
 
 def read_branches_dict(ntp, tree, branches):
-    return concatenate(f'{ntp}:{tree}', branches, library=ARRAY_TYPE)
+    return concatenate(regulate_input(ntp, tree), branches, library=ARRAY_TYPE)
 
 
 def read_branches(ntp, tree, branches, idx=None, transpose=False):
