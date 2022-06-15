fa3202c6083ce8223414a8191e7e3fd5f36db0cf
Yipeng Sun <syp@umd.edu>
2022-06-15 11:23:10 -0400

    Use mplhep and uhi from root-curated.

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -3,8 +3,6 @@ final: prev:
 {
   pythonOverrides = prev.lib.composeExtensions prev.pythonOverrides (finalPy: prevPy: {
     pyTuplingUtils = finalPy.callPackage ./default.nix { };
-    mplhep = finalPy.callPackage ./mplhep { };
-    uhi = finalPy.callPackage ./uhi { };
   });
   python3 = prev.python3.override { packageOverrides = final.pythonOverrides; };
 }
